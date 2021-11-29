539f7bd62f758d4763f0fa39a760808bbfaf602a
doomer2004 <90173692+doomer2004@users.noreply.github.com>
2021-11-29 22:28:14 +0200

    Add files via upload

---
+++ acd/lab6/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+#include<cmath>
+using namespace std;
+int Foo(int x, int q, int b)
+{
+	if (b > 0)
+		return (x) + Foo(x * q, q, b - 1);
+	else
+		return 0;
+}
+int main()
+{
+   cout << Foo(2, 2, 6);
+}
