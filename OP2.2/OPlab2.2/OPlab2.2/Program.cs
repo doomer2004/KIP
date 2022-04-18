efccd8b589919d3013e920e9f57ad675c3bce7dd
doomer2004 <90173692+doomer2004@users.noreply.github.com>
2022-04-19 00:24:33 +0300

    Add files via upload

---
+++ OP2.2/OPlab2.2/OPlab2.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OPlab2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"C:\Users\HP\Downloads\Tnum.txt";
+            string path2 = @"C:\Users\HP\Downloads\day.txt";
+            string path3 = @"C:\Users\HP\Downloads\night.txt";
+
+        }
+    }
+
+}
---
+++ OP2.2/OPlab2.2/OPlab2.2/Worker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+namespace OPlab2._2
+{
+    class Worker
+    {
+        public void CreateFile(string path)
+        {
+            FileStream fstream = new FileStream(path, FileMode.Create);
+
+            fstream.Close();
+        }
+        public void AppendToFile(string path, string calls)
+        {
+            FileStream fstream = new FileStream(path, FileMode.Append);
+            byte[] buffer = new byte[calls.Length];
+            fstream.Write(buffer, 0, calls.Length);
+            fstream.Close();
+        }
+        public string TakeColls(string path)
+        {
+            Console.WriteLine("Enter all calls:");
+            int num = int.Parse(Console.ReadLine());
+            var phoneBook = new Dictionary<string, TimeOnly>();
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Enter phone number:");
+                string pnum = Console.ReadLine();
+                Console.WriteLine("Enter time:");
+                TimeOnly time = TimeOnly.Parse(Console.ReadLine());
+
+            }
+
+            return;
+        }
+            
+    }
+}
